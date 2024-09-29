58bbd5b119ac97ee5b10632fd25228328f419f57
IsraelShowell <showellisrael0@gmail.com>
2024-09-29 17:21:42 -0400

    Version 0.20

--- app.py
+++ app.py
@@ -12,11 +12,125 @@ This web application allows users to create, log into, and manage an account, ad
 The programming for this project was begun September 27th 2024 and was finished at ==!
 """
 
-#These are the imported libaries we are using to make the program
+#These are the imported libaries I am using to make the program
+from flask import Flask, render_template, request
+from signupForm import signUpForm
 from datetime import datetime
 import sqlite3
 import random
 
+
+
+#Important variables and objects
+#Requried by Flask to detect the app when running 'flask run'
+app=Flask(__name__)
+#This is used to help protect data being sent by the app.
+#This protection is used to defend against CSRF attacks.
+#(Cross-Site Request Forgery)
+app.secret_key="__privatekey__"
+
+
+#All HTML files are located in the 'templates' because that is where render_template looks for HTML files
+
+#The Home Page is located as the root of the web page
+@app.route('/')
+def Home():
+    #Home is referenced in the HTML files
+    return render_template('Home.html')
+
+#End of Home function
+
+
+#The Login Page is able to detect POST and GET requests
+#POST sends data, GET gets data
+@app.route('/login', methods=['POST','GET'])
+def login():
+    #When the button is pressed, the user makes the page send a POST request
+    if request.method=='POST':
+        #Saves the request's data in variables
+        userName = request.form['Username']
+        passWord = request.form['Password']
+
+        #Then it connects to the database
+        login_connection = sqlite3.connect('task-management.db')
+        #c serves as a database cursor, a control structure that enables traversal over the records in a database
+        manage_cursor = login_connection.cursor()
+
+        #statement holds an SQL Query for the users table in the users database
+        #This query checks to see if the user and password entered exist in the database
+        statement=f"SELECT * from users WHERE Username='{userName}' AND Password='{passWord}';"
+
+        #We then tell the cursor to run the query
+        manage_cursor.execute(statement)
+        #manage_cursor.fetchone fetchs the next row of a query resultand returns a single tuple,
+        #Or None if no more rows are available.
+        if not manage_cursor.fetchone():
+            #If the user and password is not found, the program will not sign them in
+            return render_template("login.html")
+        else:
+            #If the login is right, they go to the dashboard page, and their name is displayed
+            return render_template('dashboard.html', name=userName)
+    else:
+        #If the user is just going to the login page, the page is rendered by the program
+         request.method=='GET'
+         return render_template("login.html")
+
+#End of Login function
+
+#The registrationForm Page is able to detect POST and GET requests
+#POST sends data, GET gets data
+@app.route('/signup', methods=['POST','GET']) 
+def signup():
+    #This creates an object named signUp based off of the form defined in the formsubmission module
+    signUp = signUpForm()
+
+    #The program connects to the database
+    signUp_connection=sqlite3.connect('task-management.db')
+    #c serves as a database cursor, a control structure that enables traversal over the records in a database
+    manage_cursor=signUp_connection.cursor()
+
+    #When the button is pressed, the user makes the page send a POST request
+    if request.method=='POST':
+        #Checks to make sure the user didn't leave the Username and Password fields blank
+        if(request.form["Username"]!="" and request.form["Password"]!=""):
+            #Saves the request's data in variables
+            E_mail = request.form['Email']
+            userName = request.form['Username']
+            passWord = request.form['Password']
+            PhoneNumber = request.form['PhoneNumber']
+            Gender = request.form['Gender']
+            Address = request.form['Address']
+            Age = request.form['Age']
+
+            #statement holds an SQL Query for the users table in the users database
+            #This query checks to see if the user, password, and email entered exist in the database
+            statement=f"SELECT * from users WHERE Username='{userName}' AND Password='{passWord}' AND Email='{E_mail}';"
+
+            #We then tell the cursor to run the query
+            manage_cursor.execute(statement)
+
+            #Stores the result of the query in the data variable
+            data=manage_cursor.fetchone()
+
+            #If the data matches both the password and username, then the user will be taken to the error page
+            if data:
+                return render_template("error.html")
+            else:
+                #If at least the Email, Username, and Password are different from what is in the database
+                if not data:
+                    #Then the user's information will be added into the database
+                    date_created = str(datetime.now())
+                    manage_cursor.execute("INSERT INTO users (Email,Username,Password,PhoneNumber,Gender,Address,Age,DateAccountCreated) VALUES (?,?,?,?,?,?,?,?,?)",(E_mail,userName,passWord,PhoneNumber,Gender,Address,Age,date_created))
+                    signUp_connection.commit()
+                    signUp_connection.close()
+                    #Then they are taken to the login page
+                    return render_template("login.html")
+                
+    #When a user first goes to the register page, the page is rendered with a fresh form
+    elif request.method=='GET':
+        return render_template('register.html',form=signUpForm)
+
+#End of Signup function
 
 
 #This is the start up function that runs when the app is ran in the command line to start
@@ -28,7 +142,7 @@ def startup():
     #Runs a query to create a table if it does not exist
     #The data parameters that the tables can handle are a text username and a text password, etc
     #PRIMARY KEY automatically adds the UNIQUE constraint!
-    manage_cursor.execute("CREATE TABLE IF NOT EXISTS users(User_ID INTEGER PRIMARY KEY, Username text NOT NULL, Password text NOT NULL, PhoneNumber INTEGER NOT NULL, Gender text NOT NULL, Address text NOT NULL, Age INTEGER NOT NULL, DateAccountCreated text NOT NULL)")
+    manage_cursor.execute("CREATE TABLE IF NOT EXISTS users(User_ID INTEGER PRIMARY KEY, Email text NOT NULL, Username text NOT NULL, Password text NOT NULL, PhoneNumber INTEGER NOT NULL, Gender text NOT NULL, Address text NOT NULL, Age INTEGER NOT NULL, DateAccountCreated text NOT NULL)")
     manage_cursor.execute("CREATE TABLE IF NOT EXISTS tasks(Task_ID INTEGER PRIMARY KEY, TaskName text NOT NULL, TaskDesc text NOT NULL, Importance INTEGER NOT NULL, Urgency INTEGER NOT NULL, TaskDeadline text NOT NULL, DateTaskCreated text NOT NULL, User_ID INTEGER NOT NULL, Username text NOT NULL, FOREIGN KEY(User_ID) REFERENCES users(User_ID), FOREIGN KEY(Username) REFERENCES users(Username))")
 
     #Then the changes are added to the database
@@ -42,16 +156,9 @@ def startup():
 
 
 
-
-
-
-
-
-
-
-
-#This is the function call
+#This needs to be outside of the __name__ part, because Flask skips over it.
 startup()
-
+if __name__=='__main__':
+    app.run()
 
 #End of Script
---
+++ signupForm.py
@@ -0,0 +1,39 @@
+# Creator: Israel Showell
+# Start Date: 9/27/2024
+# End Date: /2024
+# Project: Tasker - Task Management Application
+# Version: 0.01
+
+# Description:
+"""
+This is the signup form Python script in the task management web application!
+This script creates the signup form that users use!
+This web application allows users to create, log into, and manage an account, add tasks to their dashboard and run common functionalities on the tasks, and mark them complete!
+The programming for this project was begun September 27th 2024 and was finished at ==!
+"""
+
+#These are the imported libaries I am using to make the program
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField,IntegerField,TextAreaField,SubmitField
+from wtforms.validators import ValidationError, DataRequired
+
+#Here, I create a form format that will be rendered by app.py and by the HTML page
+class signUpForm(FlaskForm):
+    #Creates the email field and requires users to use it.
+    Email=StringField(label="Email",validators=[DataRequired()])
+    #Creates the username field and requires users to use it.
+    Username=StringField(label="Username",validators=[DataRequired()])
+    #Creates the password field and requires users to use it.
+    Password=PasswordField(label="Password",validators=[DataRequired()])
+    #Creates the phone field and requires users to use it.
+    PhoneNumber=IntegerField(label="Enter Mobile Number",validators=[DataRequired()])
+    #Creates the Gender field 
+    Gender=StringField(label="Gender")
+    #Creates the address TextAreaField 
+    Address=TextAreaField(label="Address")
+    #Creates the username field and requires users to use it.
+    Age=IntegerField(label="Age")
+    #Creates the button for users to submit their form
+    Submit=SubmitField(label="Send")
+
+#End of Script
